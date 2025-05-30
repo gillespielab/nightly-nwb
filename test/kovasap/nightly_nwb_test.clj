f3c0e925fddb39358803662a23d9f6951dbde95e
Kovas Palunas <kovas.palunas@gmail.com>
2025-05-29 18:50:55 -0700

    initial

---
+++ test/kovasap/nightly_nwb_test.clj
@@ -0,0 +1,7 @@
+(ns kovasap.nightly-nwb-test
+  (:require [clojure.test :refer :all]
+            [kovasap.nightly-nwb :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
