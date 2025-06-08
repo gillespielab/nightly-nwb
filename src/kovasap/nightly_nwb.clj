23896c503ab06a4f1abb7afe7b932e2139aee59c
Kovas Palunas <kovas.palunas@gmail.com>
2025-06-07 17:05:16 -0700

    working copying of yaml to directory!

--- src/kovasap/nightly_nwb.clj
+++ src/kovasap/nightly_nwb.clj
@@ -12,6 +12,27 @@
 
 (def temp-spreadsheet-filepath "out.xlsx")
 
+(def default-template-yaml-filepath
+  "{{path-to-raw-files}}/{{experimenter}}/{{subject}}/{{subject}}_metadata.yml")
+
+(def default-output-yaml-filepath
+  "{{path-to-raw-files}}/{{experimenter}}/{{subject}}/{{date}}/{{date}}_{{subject}}_metadata.yml")
+
+(def behavior-sheet-name
+  "{{subject}}_behavior")
+
+(def adjusting-sheet-name
+  "{{subject}}_adjusting")
+
+(defn replace-placeholders
+  [template-string arg-map]
+  (string/replace template-string
+                  #"\{\{([a-zA-Z\-]+)\}\}"
+                  (fn [[_match placeholder]]
+                    (str (get arg-map (keyword placeholder))))))
+   
+(replace-placeholders "My name is {{name}} and I am {{age}} years old." {:name "Bob" :age 25})
+
 (defn zip [& colls]
   (partition (count colls) (apply interleave colls)))
 
@@ -42,9 +63,6 @@
   (into {} (for [sheet (ss/sheet-seq (ss/load-workbook workbook-path))]
              [(.getSheetName sheet) sheet])))
 
-(defn populate-yaml-data
-  [template-yaml-data parsed-sheet-data raw-data-file-paths])
-
 (defn download-google-sheet!
   "Returns true if the download was successful, false otherwise."
   [google-sheet-id]
@@ -56,9 +74,10 @@
                                    "--overwrite")]
     (println out)
     (println err)
+    (println "Done exporting spreadsheet")
     (if (= exit 0)
-      (throw (Exception. err))
-      temp-spreadsheet-filepath)))
+      temp-spreadsheet-filepath
+      (throw (Exception. err)))))
 
 
 (defn get-raw-file-paths
@@ -66,52 +85,67 @@
   ; TODO turn these into strings instead of java files
   (file-seq (clojure.java.io/file path-to-raw-files)))
 
-(def nightly-nwb-email "")
+(defn generate-single-yaml
+  [experimenter subject date behavior-data adjusting-data template-yaml-data data-filepaths]
+  template-yaml-data)
 
-(defn send-error-email!
-  [email-to-notify error-text options]
-  (send-message {:from nightly-nwb-email
-                 :to [email-to-notify]
-                 :subject "Nightly NWB ran into issues."
-                 :body (str error-text options)}))
+(defn determine-dates-to-process
+  "Return list of dates formatted like YYYYMMDD for which there are no yaml files in their directory."
+  [experimenter subject path-to-raw-files]
+  ["20250602"])
 
-(defn send-success-email!
-  [email-to-notify options]
-  (send-message {:from nightly-nwb-email
-                 :to [email-to-notify]
-                 :subject "Nightly NWB ran successfully."
-                 :body (str options)}))
+(defn write-yaml-data-to-file
+  [yaml-data filepath]
+  (spit filepath (yaml/generate-string yaml-data)))
 
 (defn generate-yaml!
+  "Returns map like {:success? true :failure-message ''}."
   [{:keys [google-sheet-id
+           experimenter
+           subject
            yaml-template-file
            output-yaml-file
-           path-to-raw-files
-           email-to-notify] :as options}]
-  (try
-    (spit
-      output-yaml-file
-      (yaml/generate-string
-        (populate-yaml-data
-          (yaml/parse-string (slurp yaml-template-file))
-          (parse-sheets (download-google-sheet! google-sheet-id))
-          (get-raw-file-paths path-to-raw-files))))
-    (catch Exception e (send-error-email! email-to-notify e options))
-    (finally (send-success-email! email-to-notify options))))
+           path-to-raw-files]
+    :as   options}]
+  (let [parsed-sheets (parse-sheets (download-google-sheet! google-sheet-id))]
+    (println "Starting date processing...")
+    (for [date (determine-dates-to-process experimenter
+                                           subject
+                                           path-to-raw-files)
+          :let [options (assoc options :date date)]]
+      (do (println (str "Processing data for " date "..."))
+          (try (write-yaml-data-to-file
+                 (generate-single-yaml
+                   experimenter
+                   subject
+                   date
+                   (get parsed-sheets
+                        (replace-placeholders behavior-sheet-name options))
+                   (get parsed-sheets
+                        (replace-placeholders adjusting-sheet-name options))
+                   (yaml/parse-string
+                     (slurp (replace-placeholders yaml-template-file options)))
+                   (get-raw-file-paths path-to-raw-files))
+                 (replace-placeholders output-yaml-file options))
+               (catch Exception e {:success? false :failure-message e})
+               (finally {:success? true}))))))
 
 (def cli-options
    [["-g" "--google-sheet-id ID" "ID for google sheet to parse."
-     :default "11tDzUNBq9zIX6_9Rel__fdAUezAQzSnh5AVYzCP060c"]
+     :default "1QxgE1NmOHCZbkmkR0kq1E03szCnmwS7VdtZwE8eyrUY"]
     ["-f" "--path-to-raw-files DIRECTORY"
-     "The path to the raw datafiles to be packaged into the NWB file."]
+     "The path to the raw datafiles."
+     :default "banyan/raw/"]
+    ["-s" "--subject SUBJECT" "Subject to process data for."]
+    ["-e" "--experimenter EXPERIMENTER" "Experimenter to process data for."]
     ["-y" "--yaml-template-file FILE" "Template yaml file to update."
-     :default "template.yaml"
-     :validate [#(string/ends-with? % ".yaml") "Must be a .yaml file."]]
+     :default default-template-yaml-filepath
+     :validate [#(string/ends-with? % ".yml") "Must be a .yml file."]]
     ["-o" "--output-yaml-file FILE" "Output yaml file path."
-     :default "out.yaml"
-     :validate [#(string/ends-with? % ".yaml") "Must be a .yaml file."]]
-    ["-e" "--email-to-notify EMAIL" "Email address to send notification emails to."
-     :default ""
+     :default default-output-yaml-filepath
+     :validate [#(string/ends-with? % ".yml") "Must be a .yml file."]]
+    ["-n" "--email-to-notify EMAIL" "Email address to send notification emails to."
+     :default nil
      :validate [#(re-matches #".+\@.+\..+" %) "Must be a valid email."]]
     ["-h" "--help"]])
 
@@ -148,13 +182,35 @@
       :else ; failed custom validation => exit with usage summary
       {:exit-message (usage summary)})))
 
+
+(def nightly-nwb-email "")
+
+(defn send-email-report!
+  [email-to-notify status-map]
+  (send-message {:from nightly-nwb-email
+                 :to [email-to-notify]
+                 :subject "Nightly NWB ran into issues."
+                 :body ""}))
+
+(defn report-errors!
+  [email-to-notify status-map]
+  (if (nil? email-to-notify)
+    (println status-map)
+    (send-email-report!
+      email-to-notify
+      status-map)))
+
 (defn exit [status msg]
   (println msg)
   (System/exit status))
 
 (defn -main [& args]
   (let [{:keys [action options exit-message ok?]} (validate-args args)]
-    (if exit-message
-      (exit (if ok? 0 1) exit-message)
-      (case action
-        "generate-yaml"  (generate-yaml! options)))))
+    (report-errors!
+      (:email-to-notify options)
+      (if exit-message
+        (exit (if ok? 0 1) exit-message)
+        (case action
+          "generate-yaml"  (generate-yaml! options)))))
+    ; Added so that our use of sh does not hang the program.
+  (shutdown-agents))
